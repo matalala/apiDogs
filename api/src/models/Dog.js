e1d93e95eb86b02c0180e0cd76a78ec9e53b6504
matalala <smitesotra@gmail.com>
2022-07-18 19:42:41 -0300

    first commit

---
+++ api/src/models/Dog.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },{
+    timestamps:false
+  });
+};
